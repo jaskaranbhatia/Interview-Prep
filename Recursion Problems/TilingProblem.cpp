e2322883602d4537c3433d1ed2f344801595bd8a
Jaskaran Singh <jaskaransingh.singh77@gmail.com>
2020-06-06 11:59:35 +0530

    Update TilingProblem.cpp

--- Recursion Problems/TilingProblem.cpp
+++ Recursion Problems/TilingProblem.cpp
@@ -1,6 +1,12 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+/* Given a “2 x n” board and tiles of size “2 x 1”, 
+count the number of ways to tile the given board using the 2 x 1 tiles. 
+A tile can either be placed horizontally i.e., 
+as a 1 x 2 tile or vertically i.e., as 2 x 1 tile.
+*/
+
 int totalWays(int n){
     if(n<1) return 0;
     if(n<4) return 1;
